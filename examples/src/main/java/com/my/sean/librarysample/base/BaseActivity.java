e9e7489d045acf68b9877c8cb22dea4bbbffd648
Sean <sunyongjiu2000@163.com>
2018-05-28 18:31:31 +0800

    add index list

---
+++ examples/src/main/java/com/my/sean/librarysample/base/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.my.sean.librarysample.base;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+
+@SuppressLint("Registered")
+public class BaseActivity extends AppCompatActivity {
+}
